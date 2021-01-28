8a33435a5a8da24b7ac7ec79673b0f60b6e3fb23
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-01-28 13:57:21 +0100

    log in test, serializer and view

--- server/taxi/trips/serializers.py
+++ server/taxi/trips/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user, get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(write_only=True)
@@ -24,3 +25,13 @@ class UserSerializer(serializers.ModelSerializer):
             'id', 'username', 'password1', 'password2', 'first_name', 'last_name',
         )
         read_only_fields = ('id', )
+
+class LogInSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        user_data = UserSerializer(user).data
+        for key, value in user_data.items():
+            if key != 'id':
+                token[key] = value
+        return token
--- server/taxi/trips/views.py
+++ server/taxi/trips/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LogInSerializer
 
 
 # Extend Django REST Framework’s CreateAPIView and leverage on
@@ -9,3 +10,6 @@ from .serializers import UserSerializer
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
