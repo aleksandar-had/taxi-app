bc9c9d59bb9a2af78077d7905094a88368d36d64
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-01-31 13:12:51 +0100

    server send and receive broadcast group messages

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -2,10 +2,20 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
+    groups = ['test']
+
     async def connect(self):
+        await self.channel_layer.group_add(
+            group='test',
+            channel=self.channel_name
+        )
         await self.accept()
 
     async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
         await super().disconnect(code)
 
     async def receive_json(self, content, **kwargs):
@@ -15,3 +25,12 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 'type': message_type,
                 'data': content.get('data'),
             })
+
+    # When a message comes from a channel layer, Channels looks for a function
+    # on the receiving consumer whose name matches the message type . It also
+    # converts any . characters to _ characters before it checks for the match.
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -1,5 +1,7 @@
 from channels.testing import WebsocketCommunicator
 import pytest
+from channels.layers import get_channel_layer
+
 
 from taxi.routing import application
 
@@ -33,6 +35,23 @@ class TestWebSocket:
             'data': 'Test message.',
         }
         await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'Test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()
