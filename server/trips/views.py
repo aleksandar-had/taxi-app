4171f4d4710262abbc9dec6634e1f1333f2e7084
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-03-02 11:04:05 +0100

    rider group added to user model, http tests and views updated to match user model

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework import generics, permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -20,11 +21,19 @@ class LogInView(TokenObtainPairView):
 class TripView(viewsets.ReadOnlyModelViewSet):
     # tells the view to get the trip record by its id value
     lookup_field = "id"
-
     # tells the view what named parameter to use to extract
     # the id value from the URL
     lookup_url_kwarg = "trip_id"
 
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.group == "driver":
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == "rider":
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
