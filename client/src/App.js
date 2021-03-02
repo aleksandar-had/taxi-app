20ff377d8fa110ef63866033dbfcdd3394e15159
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-03-02 13:41:40 +0100

    home page with trivial routing logic, cypress environment set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
 
-import './App.css';
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
 
-function App () {
+import "./App.css";
+
+function App() {
   return (
-    <h1>Taxi</h1>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <div>
+            <h1>Taxi</h1>
+            <Link to="/sign-up">Sign Up</Link>
+            <Link to="/log-in">Log In</Link>
+          </div>
+        )}
+      />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/log-in" component={LogIn} />
+    </Switch>
   );
 }
 
