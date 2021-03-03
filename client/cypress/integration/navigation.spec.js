bd2bde54c15a0e84baf355980ccc1a964ce7cd12
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-03-03 11:57:39 +0100

    log in form and cypress test added

--- client/cypress/integration/navigation.spec.js
+++ client/cypress/integration/navigation.spec.js
@@ -35,3 +35,13 @@ describe("Navigation", function () {
     cy.hash().should("eq", "#/log-in");
   });
 });
+
+describe("Authentication", function () {
+  it("Can log in", function () {
+    cy.visit("/#/log-in");
+    cy.get("input#username").type("hey@example.com");
+    cy.get("input#password").type("pAssw0rd", { log: false });
+    cy.get("button").contains("Log in").click();
+    cy.hash().should("eq", "#/");
+  });
+});
--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Breadcrumb, Card, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { Formik } from "formik";
+import { Breadcrumb, Button, Card, Col, Form, Row } from "react-bootstrap";
 
 function LogIn(props) {
+  const [isSubmitted, setSubmitted] = useState(false);
+  const onSubmit = (values, actions) => setSubmitted(true);
+
+  if (isSubmitted) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <Row>
       <Col lg={12}>
@@ -12,7 +20,40 @@ function LogIn(props) {
         </Breadcrumb>
         <Card>
           <Card.Header>Log in</Card.Header>
-          <Card.Body></Card.Body>
+          <Card.Body>
+            <Formik
+              initialValues={{
+                username: "",
+                password: "",
+              }}
+              onSubmit={onSubmit}
+            >
+              {({ handleChange, handleSubmit, values }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <Form.Group controlId="username">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      name="username"
+                      onChange={handleChange}
+                      value={values.username}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="password">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      onChange={handleChange}
+                      value={values.password}
+                    />
+                  </Form.Group>
+                  <Button block type="submit" variant="primary">
+                    Log in
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Card.Body>
           <p className="mt-3 text-center">
             Don't have an account? <Link to="/sign-up">Sign up</Link>
           </p>
